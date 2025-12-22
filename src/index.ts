ea7f3fa7042674f1454cf99507fb9fc6809a505b
Morfis <manciajose1208@gmail.com>
2025-12-21 18:27:44 -0600

    feat: mejorar la salida de consola con colores en la conexión a MongoDB y el inicio del servidor

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import colors from "colors";
 import app from "./server";
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+  console.log(colors.blue.italic(`Server started on port ${PORT}`));
 });
