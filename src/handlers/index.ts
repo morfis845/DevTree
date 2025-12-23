47fb251f212fb17168cf6ca46ed82ca728bcf108
Morfis <manciajose1208@gmail.com>
2025-12-22 22:04:15 -0600

    feat: agregar función de hash de contraseña y actualizar registro de usuario para almacenar contraseñas hasheadas

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,14 +2,16 @@ import colors from "colors";
 import User from "../models/User";
 import { Request, Response } from "express";
 import { logger } from "../utils/logger";
+import { hashPassword } from "../utils/auth";
 
 export const registerUser = async (req: Request, res: Response) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   const userExists = await User.findOne({ email });
   if (userExists) {
     logger.warn("Intento de registro de usuario existente");
     return res.status(400).json({ message: "User already exists" });
   }
-  await User.create(req.body);
+  const hashedPassword = await hashPassword(password);
+  await User.create({ ...req.body, password: hashedPassword });
   res.status(201).json({ message: "User registered successfully" });
 };
