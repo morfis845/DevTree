ffa82624ab184f0a6048465e75bd97824fe3ed19
Morfis <manciajose1208@gmail.com>
2025-12-23 20:22:28 -0600

    feat: agregar validaciones adicionales en el registro de usuario

--- src/router.ts
+++ src/router.ts
@@ -8,6 +8,19 @@ const router = Router();
 router.post(
   "/auth/register",
   body("handle").notEmpty().withMessage("Handle is required"),
+  body("name").notEmpty().withMessage("Name is required"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long")
+    .matches(/[A-Z]/)
+    .withMessage("Password must contain at least one uppercase letter")
+    .matches(/[a-z]/)
+    .withMessage("Password must contain at least one lowercase letter")
+    .matches(/[0-9]/)
+    .withMessage("Password must contain at least one number")
+    .matches(/[!@#$%^&*(),.?":{}|<>]/)
+    .withMessage("Password must contain at least one special character"),
+  body("email").isEmail().withMessage("Valid email is required"),
   registerUser
 );
 
